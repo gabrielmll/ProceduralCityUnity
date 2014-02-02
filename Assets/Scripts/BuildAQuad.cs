22042c8c34f7bc8de3321bbc52c6b28bed83123c
Gabriel Pires e Albuquerque de Mello <gabrielmll@gmail.com>
2014-02-02 21:23:39 +0000

    Adding quadrant constructor

---
+++ Assets/Scripts/BuildAQuad.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildAQuad : MonoBehaviour {
+	public Vector2 q1;
+	public Vector2 q2;
+	public Vector2 q3;
+	public Vector2 q4;
+
+	// Use this for initialization
+	// quad parameter is the quadrant to fill with buildings
+	// q1-------q2
+	//  |       |
+	//  |		|
+	// q4-------q3
+	void Start () {
+		
+
+
+		// 1 quad become 4 buildings
+
+
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
